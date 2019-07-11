5ca6a95a34c98ae8a62c5f2d1a9444444bf1e6a5
Andrey Gaas <gaas.dron@yandex.ru>
2019-07-11 23:08:05 +0700

    Mobile menu button for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Icon } from 'react-icons-kit';
+import { th } from 'react-icons-kit/fa/th';
 import './styles.scss';
 import Button from '../Button';
 
 function Header() {
   return (
     <header className="header">
+      <Button className="button-icon">
+        <div>
+          <Icon size={30} icon={th} />
+        </div>
+      </Button>
       <a href="/" className="title">SERIALKIN</a>
       <div className="logo" />
       <span className="text">Сериалы Онлайн</span>
@@ -12,7 +19,7 @@ function Header() {
       <Button>Регистрация</Button>
       <Button green className="header-button">Вход</Button>
     </header>
-  )
+  );
 }
 
 export default Header;
